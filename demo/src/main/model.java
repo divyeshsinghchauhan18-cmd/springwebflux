2d82b85b2b360a66a1d3c47140d49caf6d8de71e
divyeshsinghchauhan18-cmd <divyeshsinghchauhan18@gmail.com>
2025-10-31 02:46:07 +0530

    Update model.java

---
+++ demo/src/main/model.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+public class Model {
+    private String message;
+
+    public Model(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
